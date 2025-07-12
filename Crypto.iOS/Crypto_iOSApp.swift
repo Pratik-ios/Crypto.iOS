f7629ead3f0f10a05f1957db674409dd6d1bdfc5
PratikiOSS <96528480+PratikiOS-Level@users.noreply.github.com>
2025-07-12 20:51:21 +0530

    Initial Commit

---
+++ Crypto.iOS/Crypto_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_iOSApp.swift
+//  Crypto.iOS
+//
+//  Created by Pratik Khopkar on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Crypto.iOSTests/Crypto_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_iOSTests.swift
+//  Crypto.iOSTests
+//
+//  Created by Pratik Khopkar on 12/07/25.
+//
+
+import Testing
+@testable import Crypto_iOS
+
+struct Crypto_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
